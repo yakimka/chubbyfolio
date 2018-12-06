d550cf7ed41d0e6794023298e7617f464e23051c
yakimka <ss.yakim@gmail.com>
2018-12-06 22:29:39 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <button
+      class="btn btn-primary btn-margin"
+      @click="check()">
+      Check
+    </button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+const API_URL = 'http://localhost:8000';
+
+export default {
+  name: 'app',
+  data () {
+    return {
+      authenticated: false,
+      message: ''
+    };
+  },
+  methods: {
+    check () {
+      const url = `${API_URL}/api/photosets/`;
+      return axios.get(url)
+        .then((response) => {
+          console.log(response.data);
+        });
+    }
+  }
+};
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
