20284b424abc4a4a92e9c10b32fa0a39316ce982
yakimka <ss.yakim@gmail.com>
2018-12-12 23:08:30 +0200

    Fix creating feedback message

--- feedback/api/serializers.py
+++ feedback/api/serializers.py
@@ -10,4 +10,7 @@ class MessageSerializer(serializers.ModelSerializer):
         fields = ('name', 'phone', 'email', 'text')
 
     def validate_phone(self, phone):
+        phone = re.sub('[^0-9]', '', phone)
+        if len(phone) != 12:
+            raise serializers.ValidationError("Номер телефона должен состоять из 12 цифр")
         return re.sub('[^0-9]', '', phone)
