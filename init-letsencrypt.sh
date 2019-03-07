be999a60e5c29bb1d1854f2bed1dfe98bf05c7d7
yakimka <ss.yakim@gmail.com>
2019-03-07 20:33:25 +0200

    Remove domain name from configs
    Now you can specify domain by "CHUBBYFOLIO_DOMAIN" environment variable
    Also you can disable staging mode by "CHUBBYFOLIO_IS_STAGING"
    environment variable (enabled by default)

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-domains=(example.com www.example.com)
+domains=($CHUBBYFOLIO_DOMAIN www.$CHUBBYFOLIO_DOMAIN)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="ss.yakim@gmail.com" # Adding a valid address is strongly recommended
-staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging=${CHUBBYFOLIO_IS_STAGING:-1} # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
