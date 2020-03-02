5583ae183b5662915afc4b637bb3f3a163a2f6d1
yakimka <ss.yakim@gmail.com>
2020-03-02 19:36:52 +0200

    Migrate from vie-cli 3 to 4

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
     '@vue/standard'
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'semi': [2, 'always'],
+    semi: [2, 'always'],
     'space-before-function-paren': 'off'
   },
   parserOptions: {
