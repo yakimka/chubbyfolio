92c7970237fd3f1593c77895007d8e7612fc6e6c
yakimka <ss.yakim@gmail.com>
2020-03-15 21:39:40 +0200

    Add throttle_scope to MessageViewSet

--- feedback/api/views.py
+++ feedback/api/views.py
@@ -5,3 +5,8 @@ from feedback.api.serializers import MessageSerializer
 
 class MessageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = MessageSerializer
+
+    def get_throttles(self):
+        self.throttle_scope = 'message'
+
+        return super().get_throttles()
