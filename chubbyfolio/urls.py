a3fa9e9595170f7294a59406bdaec2f1b8625418
yakimka <ss.yakim@gmail.com>
2020-03-15 21:31:35 +0200

    Add MessageViewSet

--- chubbyfolio/urls.py
+++ chubbyfolio/urls.py
@@ -4,17 +4,18 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
+from feedback.api.views import MessageViewSet
 from photosets.api.views import PhotosetViewSet
 
 router = SimpleRouter()
 router.register(r'photosets', PhotosetViewSet, basename='photoset')
+router.register(r'feedback/message', MessageViewSet, basename='message')
 
 urlpatterns = [
     path('flf7eiu2ocn/', admin.site.urls),
     path('api/', include([
-        path('', include(router.urls), name='photosets'),
+        path('', include(router.urls)),
         path('dynamic-settings/', include('dynamic_settings.urls'), name='dynamic_settings'),
-        path('feedback/', include('feedback.urls'), name='feedback'),
     ]), name='api')
 ]
 
--- feedback/api/views.py
+++ feedback/api/views.py
@@ -1,8 +1,7 @@
-from rest_framework import generics
+from rest_framework import viewsets, mixins
 
 from feedback.api.serializers import MessageSerializer
 
 
-class CreateMessageView(generics.CreateAPIView):
+class MessageViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
     serializer_class = MessageSerializer
-    throttle_scope = 'message'
